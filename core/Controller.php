3503f3c175ce93908e98b7af51e64c9f49695f61
Paul <nkosi.lawton@gmail.com>
2019-03-10 15:38:27 +0000

    This breaks live server. Investigating

--- core/Controller.php
+++ core/Controller.php
@@ -81,12 +81,12 @@ namespace element\mvc;
 			if(!$this->overrideView)$this->view->display();
 		}
 		else{
-			if($this->config::get("errors","redirect")) {
-				$location = $this->config::get("errors", "redirect_location");
-				$this->response->redirect($location);
-			} else {
+			// if($this->config::get("errors","redirect")) {
+			// 	$location = $this->config::get("errors", "redirect_location");
+			// 	$this->response->redirect($location);
+			// } else {
 				echo "No action with that name.";
-			}
+			// }
 		}
 	}
 	
